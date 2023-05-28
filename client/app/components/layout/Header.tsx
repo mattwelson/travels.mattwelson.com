72bc675c0ffe89beb07c286ae7d3c492ff9dca33
matt.welson <matt.welson@mtf.co.nz>
2023-05-28 14:11:19 +1200

    fix: important s

--- client/app/components/layout/Header.tsx
+++ client/app/components/layout/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="sticky top-0 grid grid-cols-layout gap-x-8 bg-white py-4 dark:bg-slate-800">
       <div className="col-start-2 justify-self-center font-serif text-xl">
-        <Link to="/">Travel</Link>
+        <Link to="/">Travels</Link>
       </div>
     </header>
   );
