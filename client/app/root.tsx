5ea72020b41c94b1e9e338d3778865e19403e69a
matt.welson <matt.welson@mtf.co.nz>
2023-07-10 11:40:05 +1200

    fix change title

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -29,6 +29,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Travels - Matt Welson</title>
         <Meta />
         <Links />
       </head>
